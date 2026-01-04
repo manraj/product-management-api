54d46c124448735d625bf7d1d06cbedf050462ef
Manraj <manraj.govindasamy@cscs.io>
2026-01-04 21:15:45 +0530

    Initial project setup.

---
+++ src/test/java/com/arivoli/product/ProductManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arivoli.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
